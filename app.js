52029ec9820b4d76a4074f4fe9861432eacbe3bd
Jarel79 <jareliqq.79@gmail.com>
2025-08-03 14:40:45 -0400

    actulizacion3

--- app.js
+++ app.js
@@ -1,16 +1,47 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-const inputAmigo = document.getElementById("amigo");
-const listaAmigos = [];
-const ulListaAmigos = document.getElementById("listaAmigos");
-const ulResultado = document.getElementById("resultado");
+let amigos = [];
+
 function agregarAmigo() {
-    listaAmigos.push(inputAmigo.value);
-    //?ulListaAmigos.innerHTML = ulListaAmigos.innerHTML + inputAmigo.value;
-    ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;
+  // Capturar el valor del campo de entrada
+  let nombreAmigo = document.getElementById("amigo").value;
+  
+  // Validar la entrada
+  if (nombreAmigo.trim() === "") {
+    alert("Por favor, inserte un nombre.");
+    return;
+  }
+  
+  // Actualizar el array de amigos
+  amigos.push(nombreAmigo);
+  
+  actualizarListaAmigos(amigos);
+  
+  // Limpiar el campo de entrada
+  document.getElementById("amigo").value = "";
+}
+
+function actualizarListaAmigos(amigos) {
+  // Obtener el elemento de la lista
+  const lista = document.getElementById('listaAmigos');
+  
+  lista.innerHTML = "";
+  
+  for (let i = 0; i < amigos.length; i++) {
+    const li = document.createElement('li');
+    li.textContent = amigos[i];
+    lista.appendChild(li);
+  }
 }
 
 function sortearAmigo() {
-    const random = Math.floor(Math.random() * listaAmigos.length);
-    const amigoSecreto = listaAmigos[random];
-    ulResultado.innerHTML = `<li>El amigo secreto es: ${amigoSecreto}</li>;`
+  if (amigos.length === 0) {
+    alert("No hay amigos en la lista para sortear.");
+    return;
+  }
+  
+  const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+  
+  const amigoSorteado = amigos[indiceAleatorio];
+  
+  const resultado = document.getElementById('resultado');
+  resultado.innerHTML = `<li>${amigoSorteado}</li>`;
 }
